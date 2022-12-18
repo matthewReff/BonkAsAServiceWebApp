f3504768db895eff494251808802f3771650e88c
Matthew Reff <geniusammyralltheothernamesare@gmail.com>
2022-12-18 15:45:18 -0600

    Add super basic selection box + installed materialUI

--- jailbot-web/src/App.js
+++ jailbot-web/src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import FormControl from '@mui/material/FormControl'
+import InputLabel from '@mui/material/InputLabel'
+import Select, { SelectChangeEvent } from '@mui/material/Select'
+import MenuItem from '@mui/material/MenuItem'
+import * as React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <PageHeader />
+        <ServerSelector />
+        <OffenseSelector />
+        <SubmitButton />
     </div>
   );
+}
+
+function PageHeader() {
+    return (
+        <div>
+            <header>
+                <p>Bonk Bot Webapp</p>
+            </header>
+        </div>
+    )
+}
+
+function ServerSelector() {
+    const [age, setAge] = React.useState('');
+
+    const handleChange = (event: SelectChangeEvent) => {
+        setAge(event.target.value);
+    };
+
+    return (
+        <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Server</InputLabel>
+            <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={age}
+                label="Sever"
+                onChange={handleChange}
+            >
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+            </FormControl>
+    )
+}
+function OffenseSelector() {
+    return (
+        <div>
+            <p>Offense Thing</p>
+        </div>
+    )
+}
+function SubmitButton() {
+    return (
+        <div>
+            <p>Submit Thing</p>
+        </div>
+    )
 }
 
 export default App;
